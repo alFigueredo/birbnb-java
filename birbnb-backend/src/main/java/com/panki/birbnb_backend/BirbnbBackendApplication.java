56e9d14803e59f15458437d9be9ef98ed3c1f39e
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-08-10 18:46:36 -0300

    Initial backend commit

---
+++ birbnb-backend/src/main/java/com/panki/birbnb_backend/BirbnbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.panki.birbnb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirbnbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirbnbBackendApplication.class, args);
+	}
+
+}
---
+++ birbnb-backend/src/test/java/com/panki/birbnb_backend/BirbnbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panki.birbnb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirbnbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
